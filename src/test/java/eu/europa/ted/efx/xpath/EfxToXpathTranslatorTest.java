05102e44259b401eb58dd316898dc5481d584048
ROUSSY Christophe (OP-EXT) <Christophe.ROUSSY@ext.publications.europa.eu>
2022-02-22 18:56:43 +0100

    TEDEFO-617 efxt.g4 grammar and translator (work in progress) to translate to XSLT.

--- src/test/java/eu/europa/ted/efx/xpath/EfxToXpathTranslatorTest.java
+++ src/test/java/eu/europa/ted/efx/xpath/EfxToXpathTranslatorTest.java
@@ -1,6 +1,7 @@
 package eu.europa.ted.efx.xpath;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 public class EfxToXpathTranslatorTest {
@@ -10,4 +11,12 @@ public class EfxToXpathTranslatorTest {
   public void testTranslateConditionAlways() {
     assertEquals("true", EfxToXpathTranslator.translateCondition("ALWAYS"));
   }
+
+  @Test
+  @SuppressWarnings("static-method")
+  public void testTemplateParsing() throws IOException {
+    String output = EfxtToXsltTranslator.translateTemplateFile("src/test/resources/efxt-test1.efxt");
+    System.out.println(output);
+  }
+
 }
