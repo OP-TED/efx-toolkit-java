7d3b9667593a35b46b8b4e4a08278216577e02fa
ROUSSY Christophe (OP-EXT) <Christophe.ROUSSY@ext.publications.europa.eu>
2022-03-22 19:34:34 +0100

    TEDEFO-617 removing unused imports.

--- src/main/java/eu/europa/ted/efx/labels/LabelMapImpl.java
+++ src/main/java/eu/europa/ted/efx/labels/LabelMapImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import javax.management.RuntimeErrorException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
--- src/test/java/eu/europa/ted/efx/xpath/LabelMapTest.java
+++ src/test/java/eu/europa/ted/efx/xpath/LabelMapTest.java
@@ -1,7 +1,6 @@
 package eu.europa.ted.efx.xpath;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.io.IOException;
 import java.util.Locale;
