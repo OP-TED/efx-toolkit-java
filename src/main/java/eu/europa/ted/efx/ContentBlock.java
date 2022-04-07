cc31485b2b4c7dfc618909c655dfe3a90970de3a
Ioannis ROUSOCHATZAKIS <ioannis.rousochatzakis@publications.europa.eu>
2022-04-07 02:13:11 +0200

    Added basic automatic outline numbering

--- src/main/java/eu/europa/ted/efx/ContentBlock.java
+++ src/main/java/eu/europa/ted/efx/ContentBlock.java
@@ -12,6 +12,7 @@ public class ContentBlock {
     final String content;
     final Context context;
     final Queue<ContentBlock> children = new LinkedList<>();
+    final int number;
 
     private ContentBlock() {
         this.parent = null;
@@ -19,15 +20,17 @@ public class ContentBlock {
         this.indentationLevel = -1;
         this.content = "";
         this.context = null;
+        this.number = 0;
     }
 
-    public ContentBlock(final ContentBlock parent, final String id, final String content,
+    public ContentBlock(final ContentBlock parent, final String id, final int number, final String content,
             Context contextPath) {
         this.parent = parent;
         this.id = id;
         this.indentationLevel = parent.indentationLevel + 1;
         this.content = content;
         this.context = contextPath;
+        this.number = number;
     }
 
     public static ContentBlock newRootBlock() {
@@ -35,8 +38,9 @@ public class ContentBlock {
     }
 
     public ContentBlock addChild(final String content, final Context context) {
-        String newBlockId = String.format("%s%02d", this.id, this.children.size() + 1);
-        ContentBlock newBlock = new ContentBlock(this, newBlockId, content, context);
+        final int number = children.size() + 1;
+        String newBlockId = String.format("%s%02d", this.id, number);
+        ContentBlock newBlock = new ContentBlock(this, newBlockId, number, content, context);
         this.children.add(newBlock);
         return newBlock;
     }
@@ -59,6 +63,13 @@ public class ContentBlock {
         return targetBlock;
     }
 
+    public String getOutlineNumber() {
+        if (this.parent == null || this.parent.number == 0) {
+            return String.format("%d", this.number);
+        }
+        return String.format("%s.%d",  this.parent.getOutlineNumber(), this.number);
+    }
+
     public String renderContent(Renderer renderer) {
         StringBuilder sb = new StringBuilder();
         sb.append(this.content);
@@ -69,7 +80,7 @@ public class ContentBlock {
     }
 
     public void renderTemplate(Renderer renderer, List<String> templates) {
-        templates.add(renderer.renderTemplate(this.id, this.renderContent(renderer)));
+        templates.add(renderer.renderTemplate(this.id, this.getOutlineNumber(), this.renderContent(renderer)));
         for (ContentBlock child : this.children) {
             child.renderTemplate(renderer, templates);
         }
