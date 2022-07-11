3071da8eecc9b9357c54f2b06573df22590590bf
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-07-11 12:02:49 +0200

    [Multi-SDK]: Instantiate an annotated implementation based on constructor parameter types declared on the annotation.

--- src/main/java/eu/europa/ted/efx/interfaces/EfxExpressionTranslator.java
+++ src/main/java/eu/europa/ted/efx/interfaces/EfxExpressionTranslator.java
@@ -1,10 +1,5 @@
 package eu.europa.ted.efx.interfaces;
 
-import org.antlr.v4.runtime.BaseErrorListener;
-
 public interface EfxExpressionTranslator {
-  EfxExpressionTranslator init(SymbolResolver symbolResolver, ScriptGenerator scriptGenerator,
-      BaseErrorListener errorListener);
-
   String translateExpression(final String context, final String expression);
 }
