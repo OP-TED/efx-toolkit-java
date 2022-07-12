75f42c1163bad0609599301c6b364a3a4ad1b8af
Ioannis ROUSOCHATZAKIS <ioannis.rousochatzakis@publications.europa.eu>
2022-07-12 03:47:18 +0200

    Moved EfxTranslator back to its place.

--- src/main/java/eu/europa/ted/efx/EfxTranslator.java
+++ src/main/java/eu/europa/ted/efx/EfxTranslator.java
@@ -1,9 +1,10 @@
-package eu.europa.ted.efx.translator;
+package eu.europa.ted.efx;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import eu.europa.ted.efx.interfaces.TranslatorDependencyFactory;
+import eu.europa.ted.efx.selector.component.EfxTranslatorFactory;
 
 public class EfxTranslator {
   enum KnownSdkVersions {
