ed1126aac4bd1b086c7db04f453fcf9240dacb1c
Ioannis ROUSOCHATZAKIS <ioannis.rousochatzakis@publications.europa.eu>
2022-08-01 13:39:09 +0200

    Fixed component selector

--- src/main/java/eu/europa/ted/efx/EfxTranslator.java
+++ src/main/java/eu/europa/ted/efx/EfxTranslator.java
@@ -7,63 +7,32 @@ import eu.europa.ted.efx.interfaces.TranslatorDependencyFactory;
 import eu.europa.ted.efx.selector.component.EfxTranslatorFactory;
 
 public class EfxTranslator {
-  enum KnownSdkVersions {
-    SDK_0_6("0.6"), SDK_0_7("0.7"), SDK_1("1"), UNSUPPORTED("N/A");
-
-    private String name;
-
-    private KnownSdkVersions(String name) {
-      this.name = name;
-    }
-
-    public String getName() {
-      return name;
-    }
-  }
-
   public static String translateExpression(final String context, final String expression,
       final TranslatorDependencyFactory dependencyFactory, final String sdkVersion)
       throws InstantiationException {
     return EfxTranslatorFactory
-        .getEfxExpressionTranslator(normalizeVersion(sdkVersion).getName(), dependencyFactory)
+        .getEfxExpressionTranslator(sdkVersion, dependencyFactory)
         .translateExpression(context, expression);
   }
 
   public static String translateTemplate(final Path pathname,
       final TranslatorDependencyFactory dependencyFactory, final String sdkVersion)
       throws IOException, InstantiationException {
-    return EfxTranslatorFactory.getEfxTemplateTranslator(normalizeVersion(sdkVersion).getName(), dependencyFactory)
+    return EfxTranslatorFactory.getEfxTemplateTranslator(sdkVersion, dependencyFactory)
         .renderTemplate(pathname);
   }
 
   public static String translateTemplate(final String template,
       final TranslatorDependencyFactory dependencyFactory, final String sdkVersion)
       throws InstantiationException {
-    return EfxTranslatorFactory.getEfxTemplateTranslator(normalizeVersion(sdkVersion).getName(), dependencyFactory)
+    return EfxTranslatorFactory.getEfxTemplateTranslator(sdkVersion, dependencyFactory)
         .renderTemplate(template);
   }
 
   public static String translateTemplate(final InputStream stream,
       final TranslatorDependencyFactory dependencyFactory, final String sdkVersion)
       throws IOException, InstantiationException {
-    return EfxTranslatorFactory.getEfxTemplateTranslator(normalizeVersion(sdkVersion).getName(), dependencyFactory)
+    return EfxTranslatorFactory.getEfxTemplateTranslator(sdkVersion, dependencyFactory)
         .renderTemplate(stream);
-  }
-
-  private static KnownSdkVersions normalizeVersion(final String sdkVersion) {
-    String normalizedVersion = sdkVersion;
-
-    if (normalizedVersion.startsWith("eforms-sdk-")) {
-      normalizedVersion = normalizedVersion.substring(11);
-    }
-    String[] numbers = normalizedVersion.split("\\.", -2);
-    normalizedVersion = "SDK_" + (numbers.length > 0 ? numbers[0] : "") 
-        + ((numbers.length > 1 && Integer.parseInt(numbers[0]) > 0) ? "" : "_" + numbers[1]);
-
-    try {
-      return KnownSdkVersions.valueOf(normalizedVersion);
-    } catch (IllegalArgumentException e) {
-      return KnownSdkVersions.UNSUPPORTED;
-    }
   }
 }
