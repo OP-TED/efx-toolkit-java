17f59fa50aee79722dc2d65efaac7052fee1657d
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2023-04-18 16:46:22 +0200

    [EfxExpressionCombinedTest]: Execute tests for both SDK 1 and SDK 2.

--- src/test/java/eu/europa/ted/efx/EfxExpressionCombinedTest.java
+++ src/test/java/eu/europa/ted/efx/EfxExpressionCombinedTest.java
@@ -1,49 +1,80 @@
 package eu.europa.ted.efx;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import eu.europa.ted.efx.mock.DependencyFactoryMock;
 
 /**
  * Test for EFX expressions that combine several aspects of the language.
  */
 class EfxExpressionCombinedTest {
-  final private String SDK_VERSION = "eforms-sdk-2.0";
+  private static final String[] SDK_VERSIONS = new String[] {"eforms-sdk-1.0", "eforms-sdk-2.0"};
 
-  private String test(final String context, final String expression) {
+  private static Stream<Arguments> provideSdkVersions() {
+    List<Arguments> arguments = new ArrayList<>();
+
+    for (String sdkVersion : SDK_VERSIONS) {
+      arguments.add(Arguments.of(sdkVersion));
+    }
+
+    return Stream.of(arguments.toArray(new Arguments[0]));
+  }
+
+  private void testExpressionTranslationWithContext(final String sdkVersion,
+      final String expectedTranslation, final String context, final String expression) {
+    assertEquals(expectedTranslation,
+        translateExpressionWithContext(sdkVersion, context, expression));
+  }
+
+  private String translateExpressionWithContext(String sdkVersion, final String context,
+      final String expression) {
     try {
-      return EfxTranslator.translateExpression(DependencyFactoryMock.INSTANCE,
-          SDK_VERSION, String.format("{%s} ${%s}", context, expression));
+      return EfxTranslator.translateExpression(DependencyFactoryMock.INSTANCE, sdkVersion,
+          String.format("{%s} ${%s}", context, expression));
     } catch (InstantiationException e) {
       throw new RuntimeException(e);
     }
   }
 
-  @Test
-  void testNotAnd() {
-    assertEquals("not(1 = 2) and (2 = 2)", test("BT-00-Text", "not(1 == 2) and (2 == 2)"));
+  @ParameterizedTest
+  @MethodSource("provideSdkVersions")
+  void testNotAnd(final String sdkVersion) {
+    testExpressionTranslationWithContext(sdkVersion, "not(1 = 2) and (2 = 2)", "BT-00-Text",
+        "not(1 == 2) and (2 == 2)");
   }
 
-  @Test
-  void testNotPresentAndNotPresent() {
-    assertEquals("not(PathNode/TextField) and not(PathNode/IntegerField)",
-        test("ND-Root", "BT-00-Text is not present and BT-00-Integer is not present"));
+  @ParameterizedTest
+  @MethodSource("provideSdkVersions")
+  void testNotPresentAndNotPresent(final String sdkVersion) {
+    testExpressionTranslationWithContext(sdkVersion,
+        "not(PathNode/TextField) and not(PathNode/IntegerField)", "ND-Root",
+        "BT-00-Text is not present and BT-00-Integer is not present");
   }
 
-  @Test
-  void testCountWithNodeContextOverride() {
-    assertEquals("count(../../PathNode/CodeField) = 1",
-        test("BT-00-Text", "count(ND-Root::BT-00-Code) == 1"));
+  @ParameterizedTest
+  @MethodSource("provideSdkVersions")
+  void testCountWithNodeContextOverride(final String sdkVersion) {
+    testExpressionTranslationWithContext(sdkVersion, "count(../../PathNode/CodeField) = 1",
+        "BT-00-Text", "count(ND-Root::BT-00-Code) == 1");
   }
 
-  @Test
-  void testCountWithAbsoluteFieldReference() {
-    assertEquals("count(/*/PathNode/CodeField) = 1", test("BT-00-Text", "count(/BT-00-Code) == 1"));
+  @ParameterizedTest
+  @MethodSource("provideSdkVersions")
+  void testCountWithAbsoluteFieldReference(final String sdkVersion) {
+    testExpressionTranslationWithContext(sdkVersion, "count(/*/PathNode/CodeField) = 1",
+        "BT-00-Text", "count(/BT-00-Code) == 1");
   }
 
-  @Test
-  void testCountWithAbsoluteFieldReferenceAndPredicate() {
-    assertEquals("count(/*/PathNode/CodeField[../IndicatorField = true()]) = 1",
-        test("BT-00-Text", "count(/BT-00-Code[BT-00-Indicator == TRUE]) == 1"));
+  @ParameterizedTest
+  @MethodSource("provideSdkVersions")
+  void testCountWithAbsoluteFieldReferenceAndPredicate(final String sdkVersion) {
+    testExpressionTranslationWithContext(sdkVersion,
+        "count(/*/PathNode/CodeField[../IndicatorField = true()]) = 1", "BT-00-Text",
+        "count(/BT-00-Code[BT-00-Indicator == TRUE]) == 1");
   }
 }
