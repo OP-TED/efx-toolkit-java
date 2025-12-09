e20dacf6e1827bc611f0b32cbb6f84514049d673
Ioannis Rosuochatzakis <ioannis.rousochatzakis@publications.europa.eu>
2025-12-09 04:42:54 +0100

    Fix license text in multiple interface files

--- src/main/java/eu/europa/ted/efx/interfaces/EfxExpressionTranslator.java
+++ src/main/java/eu/europa/ted/efx/interfaces/EfxExpressionTranslator.java
@@ -9,7 +9,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the Licence
  * is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the Licence for the specific language governing permissions and limitations under
- * the Lic
+ * the Licence.
  */
 
 package eu.europa.ted.efx.interfaces;
